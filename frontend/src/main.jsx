9083ac22e6f7e22aeec8b2adc5b3d436386adf91
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-01 07:08:08 -0600

    refactor: Add ProfileProvider for managing profile state

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import './index.css';
 
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ProfileProvider } from './contexts/ProfileContext.jsx';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
-		<App />
+		<ProfileProvider>
+			<App />
+		</ProfileProvider>
 	</BrowserRouter>
 );
