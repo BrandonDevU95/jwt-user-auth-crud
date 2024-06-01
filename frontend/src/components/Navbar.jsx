f6e5490a0c652c53b7670a459bbf585cf3bbc820
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-01 07:48:27 -0600

    refactor: Update Navbar component to use react-router-dom for navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+import { Link } from 'react-router-dom';
 import Profile from './Profile';
 import { getUserSession } from '../utils/userSession';
 import { useProfile } from '../contexts/ProfileContext';
@@ -58,31 +59,31 @@ const Navbar = () => {
 						{user ? (
 							<>
 								<li>
-									<a
-										href="/"
+									<Link
+										to="/"
 										className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
 										aria-current="page">
 										Home
-									</a>
+									</Link>
 								</li>
 								<Profile user={user} />
 							</>
 						) : (
 							<>
 								<li>
-									<a
-										href="/"
+									<Link
+										to="/"
 										className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
 										aria-current="page">
 										Home
-									</a>
+									</Link>
 								</li>
 								<li>
-									<a
-										href="/login"
+									<Link
+										to="/login"
 										className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 										login
-									</a>
+									</Link>
 								</li>
 							</>
 						)}
