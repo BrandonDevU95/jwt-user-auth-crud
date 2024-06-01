086e54013dd0559ba3deac3ea0379f8adb60204f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-01 08:20:09 -0600

    refactor: Update HomePage component to display user session information

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { getUserSession } from '../utils/userSession';
 import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
@@ -7,13 +8,21 @@ const HomePage = () => {
 		navigate('/login');
 	};
 
+	const userSession = getUserSession();
+
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-gray-100">
-			<div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+		<div className="flex items-center justify-center h-[calc(100vh-65px)]">
+			<div className="max-w-md py-4 px-8 bg-gray-100 shadow-lg rounded-lg my-20">
 				<div className="text-center">
 					<h2 className="text-gray-800 text-3xl font-semibold">
 						JWT Admin Panel
 					</h2>
+					{userSession && (
+						<p className="mt-2 text-gray-600">
+							Welcome, {userSession.firstname}{' '}
+							{userSession.lastname} !
+						</p>
+					)}
 					<p className="mt-2 text-gray-600">
 						Este es un ejemplo de un panel de administración con JWT
 						que utiliza React, Tailwind CSS y Express.js.
@@ -23,7 +32,7 @@ const HomePage = () => {
 					<button
 						onClick={handleLogin}
 						className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none">
-						Login
+						{userSession ? 'Profile' : 'Login'}
 					</button>
 				</div>
 			</div>
