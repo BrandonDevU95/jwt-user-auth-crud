496d130c837d63ee8dabef58c618fcc4097d1cee
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-04 12:05:09 -0600

    refactor: Remove unused import in user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,3 @@
-const user = require('../models/user');
 const User = require('../models/user');
 const { validateUser, validateUserPartial } = require('../schemas/user');
 const { encryptPassword } = require('../utils/authPass');
