985f938d5047b2d1c06b04ada43238fc660b3987
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-04 11:39:48 -0600

    refactor: Update AdminRoutes to include HomePage and ProfilePage

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,11 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AdminPage from '../pages/AdminPage';
+import HomePage from '../pages/HomePage';
+import ProfilePage from '../pages/ProfilePage';
 
 const AdminRoutes = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<AdminPage />} />
+			<Route path="/" element={<HomePage />} />
+			<Route path="/profile" element={<ProfilePage />} />
 			<Route path="/admin" element={<AdminPage />} />
 			<Route path="*" element={<Navigate to="/" replace />} />
 		</Routes>
