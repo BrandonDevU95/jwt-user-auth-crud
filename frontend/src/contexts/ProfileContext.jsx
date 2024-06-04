f9736453e559fa5a7035982ad929a7bd7c96cec5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-04 11:39:11 -0600

    refactor: Add user authentication check in ProfileContext

--- frontend/src/contexts/ProfileContext.jsx
+++ frontend/src/contexts/ProfileContext.jsx
@@ -1,12 +1,48 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
 	const [reloadProfile, setReloadProfile] = useState(false);
+	const [status, setStatus] = useState('no-authenticated');
+	const [userRefresh, setUserRefresh] = useState(false);
+
+	useEffect(() => {
+		setUserRefresh(false);
+		(async () => {
+			try {
+				const response = await fetch(
+					'http://localhost:3000/api/user/role',
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						credentials: 'include',
+					}
+				);
+
+				const data = await response.json();
+
+				if (!response.ok || !data.role) {
+					setStatus('no-authenticated');
+				} else {
+					setStatus(data.role);
+				}
+			} catch (error) {
+				console.error('Failed to authenticate', error);
+			}
+		})();
+	}, [userRefresh]);
 
 	return (
-		<ProfileContext.Provider value={{ reloadProfile, setReloadProfile }}>
+		<ProfileContext.Provider
+			value={{
+				reloadProfile,
+				setReloadProfile,
+				status,
+				setUserRefresh,
+			}}>
 			{children}
 		</ProfileContext.Provider>
 	);
