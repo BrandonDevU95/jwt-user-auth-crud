18dc557f5a7a40320c0990ef97951778bbc93f9f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-04 12:04:30 -0600

    refactor: Update user routes and controller

--- backend/router/user.js
+++ backend/router/user.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const UserController = require('../controllers/user');
 const { userAuth } = require('../middlewares/authenticated');
-const { isAdmin } = require('../middlewares/credentials');
 
 const api = express.Router();
 
 api.get('/user/me', [userAuth], UserController.getMe);
-api.post('/user/update', [userAuth], UserController.updateProfile);
-api.get('/users', [userAuth, isAdmin], UserController.getUsers);
-api.post('/user', [userAuth, isAdmin], UserController.createUser);
-api.patch('/user/:id', [userAuth, isAdmin], UserController.updateUser);
-api.delete('/user/:id', [userAuth, isAdmin], UserController.deleteUser);
 api.get('/user/role', [userAuth], UserController.userRole);
+api.post('/user/update', [userAuth], UserController.updateProfile);
 
 module.exports = api;
