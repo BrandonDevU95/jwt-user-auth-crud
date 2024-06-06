7c3521132bab9bab0a928830ea8d51434efb6016
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-05 21:59:53 -0600

    refactor: Update admin routes to use AdminController instead of UserController

--- backend/router/admin.js
+++ backend/router/admin.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const UserController = require('../controllers/user');
+const AdminController = require('../controllers/admin');
 const { userAuth } = require('../middlewares/authenticated');
 const { isAdmin } = require('../middlewares/credentials');
 
 const api = express.Router();
 
-api.get('/admin/users', [userAuth, isAdmin], UserController.getUsers);
-api.post('/admin/user', [userAuth, isAdmin], UserController.createUser);
-api.patch('/admin/user/:id', [userAuth, isAdmin], UserController.updateUser);
-api.delete('/admin/user/:id', [userAuth, isAdmin], UserController.deleteUser);
+api.get('/admin/users', [userAuth, isAdmin], AdminController.getUsers);
+api.post('/admin/user', [userAuth, isAdmin], AdminController.createUser);
+api.patch('/admin/user/:id', [userAuth, isAdmin], AdminController.updateUser);
+api.delete('/admin/user/:id', [userAuth, isAdmin], AdminController.deleteUser);
 
 module.exports = api;
