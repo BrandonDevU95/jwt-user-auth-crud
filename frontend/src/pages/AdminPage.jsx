650150e5959ebce0e4e0dbb2e7e0ef60cec1c56e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-05 18:13:12 -0600

    refactor: Fetch and display list of users in AdminPage

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import ListUsers from '../components/ListUsers';
+import { showToast } from '../components/Toast';
+
 const AdminPage = () => {
+	const [listUsers, setListUsers] = useState(null);
+
+	const fetchUsers = async () => {
+		try {
+			const response = await fetch(
+				'http://localhost:3000/api/admin/users',
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					credentials: 'include',
+				}
+			);
+			const data = await response.json();
+			setListUsers(data);
+		} catch (error) {
+			showToast(error.message || 'Error to getting users', 'error');
+		}
+	};
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
 	return (
-		<div>
-			<h1>Dashboard</h1>
+		<div className="container py-5 h-[calc(100vh-65px)] mx-auto">
+			{listUsers ? <ListUsers listUsers={listUsers} /> : <NoUsers />}
 		</div>
 	);
 };
 
 export default AdminPage;
+
+const NoUsers = () => {
+	return (
+		<div className="flex items-center justify-center h-full">
+			<p className="text-2xl">No users found</p>
+		</div>
+	);
+};
